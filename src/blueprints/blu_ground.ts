eda3ae621f02eb2068c1f99a78d9e59413bfc24e
Michał Budzyński <michal@virtualdesign.pl>
2020-08-18 13:55:46 +0200

    use texture on blueprints

--- src/blueprints/blu_ground.ts
+++ src/blueprints/blu_ground.ts
@@ -1,6 +1,6 @@
 import {float} from "../../common/random.js";
 import {collide} from "../components/com_collide.js";
-import {render_diffuse} from "../components/com_render_diffuse.js";
+import {render_textured} from "../components/com_render_textured.js";
 import {rigid_body} from "../components/com_rigid_body.js";
 import {Blueprint} from "../core.js";
 import {Game, Layer} from "../game.js";
@@ -12,7 +12,13 @@ export function blueprint_ground(game: Game, size: number): Blueprint {
         Children: [
             {
                 Translation: [0, float(-0.2, 0.2), 0],
-                Using: [render_diffuse(game.MaterialDiffuseGouraud, game.MeshCube, [1, 1, 0.3, 1])],
+                Using: [
+                    render_textured(
+                        game.MaterialTextured,
+                        game.MeshCube,
+                        game.Textures["grass.png"]
+                    ),
+                ],
             },
         ],
     };
--- src/blueprints/blu_player.ts
+++ src/blueprints/blu_player.ts
@@ -3,7 +3,7 @@ import {control_player} from "../components/com_control_player.js";
 import {light_point} from "../components/com_light.js";
 import {move} from "../components/com_move.js";
 import {named} from "../components/com_named.js";
-import {render_diffuse} from "../components/com_render_diffuse.js";
+import {render_textured} from "../components/com_render_textured.js";
 import {rigid_body} from "../components/com_rigid_body.js";
 import {Blueprint} from "../core.js";
 import {Game, Layer} from "../game.js";
@@ -20,7 +20,13 @@ export function blueprint_player(game: Game): Blueprint {
         Children: [
             {
                 // Body.
-                Using: [render_diffuse(game.MaterialDiffuseGouraud, game.MeshCube, [1, 1, 0.3, 1])],
+                Using: [
+                    render_textured(
+                        game.MaterialTextured,
+                        game.MeshCube,
+                        game.Textures["checker1.png"]
+                    ),
+                ],
             },
             {
                 // Camera rig anchor.
