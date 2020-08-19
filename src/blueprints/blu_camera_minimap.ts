542388c13637992634ee407e32175e746669aa07
Staś Małolepszy <stas@piesku.com>
2020-08-19 12:32:58 +0200

    Rotate the minimap camera to account for the inverted UVs

--- src/blueprints/blu_camera_minimap.ts
+++ src/blueprints/blu_camera_minimap.ts
@@ -4,8 +4,8 @@ import {Game} from "../game.js";
 
 export function blueprint_camera_minimap(game: Game): Blueprint {
     return {
-        // 90x, 0y, 0z
-        Rotation: [0.707, 0, 0, 0.707],
+        // 90x, 180y, 0z
+        Rotation: [0, 0.707, -0.707, 0],
         Children: [
             {
                 Rotation: [0, 1, 0, 0],
