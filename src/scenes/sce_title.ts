30dde9cef5ecef90ad2599a8ddf068fde253e815
Staś Małolepszy <stas@piesku.com>
2020-08-22 00:30:05 +0200

    move the camera a bit higher in stages

--- src/scenes/sce_title.ts
+++ src/scenes/sce_title.ts
@@ -51,7 +51,7 @@ export function scene_title(game: Game) {
 
     // Main Camera.
     instantiate(game, {
-        ...blueprint_camera_follow(game),
+        ...blueprint_camera_follow(game, -2),
         Translation: [float(-20, 20), float(10, 20), float(10, 20)],
         Rotation: from_euler([0, 0, 0, 0], 0, float(-135, -225), 0),
     });
