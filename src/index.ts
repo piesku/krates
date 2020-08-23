b15a0e3688fb8f56f32a3607ef4948732287ff6a
Staś Małolepszy <stas@piesku.com>
2020-08-23 11:07:35 +0200

    Skip splash screen for faster dev feedback

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {create_texture_from} from "../common/texture.js";
 import {dispatch} from "./actions.js";
 import {loop_start} from "./core.js";
 import {Game} from "./game.js";
+import {scene_title} from "./scenes/sce_title.js";
 
 let game = new Game();
 
@@ -15,4 +16,8 @@ let textures = document.querySelectorAll("img");
 for (let i = 0; i < textures.length; i++) {
     game.Textures[textures[i].id] = create_texture_from(game.Gl, textures[i]);
 }
+
+// Skip splash for faster dev feedback.
+scene_title(game);
+
 loop_start(game);
