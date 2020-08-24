c2f9cd8150b70662a2ef5e89dd3ae99d8c4a73f3
Staś Małolepszy <stas@piesku.com>
2020-08-24 23:17:07 +0200

    Fix quad UVs to account for the screen dimensions

--- materials/mat1_postprocess.ts
+++ materials/mat1_postprocess.ts
@@ -20,11 +20,8 @@ let fragment = `
 
     varying vec2 vert_texcoord;
 
-    const float aspect = 256.0 / 192.0;
-
     void main() {
-        vec2 uv = vec2(vert_texcoord.x, vert_texcoord.y / aspect + (1.0 - 1.0 / aspect));
-        gl_FragColor = texture2D(sampler, uv);
+        gl_FragColor = texture2D(sampler, vert_texcoord);
         if (mod(gl_FragCoord.x, 3.0) < 1.0 || mod(gl_FragCoord.y, 3.0) < 1.0) {
             gl_FragColor = vec4(gl_FragColor.rgb * 0.5, 1.0);
         }
