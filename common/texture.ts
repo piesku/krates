925209a2f5f674b6040b12f3d1391ff58f73f22d
Staś Małolepszy <stas@piesku.com>
2020-08-24 12:29:03 +0200

    Remove non-power-of-2 texture path; comment out default texture settings

--- common/texture.ts
+++ common/texture.ts
@@ -1,20 +1,11 @@
 import {Game} from "../src/game.js";
 import {
     GL_DATA_UNSIGNED_BYTE,
-    GL_DATA_UNSIGNED_INT,
-    GL_DEPTH_COMPONENT,
-    GL_DEPTH_COMPONENT16,
     GL_LINEAR,
-    GL_NEAREST,
-    GL_NEAREST_MIPMAP_LINEAR,
     GL_PIXEL_UNSIGNED_BYTE,
-    GL_REPEAT,
     GL_RGBA,
     GL_TEXTURE_2D,
-    GL_TEXTURE_MAG_FILTER,
     GL_TEXTURE_MIN_FILTER,
-    GL_TEXTURE_WRAP_S,
-    GL_TEXTURE_WRAP_T,
 } from "./webgl.js";
 
 export function fetch_image(path: string): Promise<HTMLImageElement> {
@@ -31,23 +22,14 @@ export function create_texture_from(gl: WebGLRenderingContext, image: HTMLImageE
     gl.texImage2D(GL_TEXTURE_2D, 0, GL_RGBA, GL_RGBA, GL_PIXEL_UNSIGNED_BYTE, image);
 
     // WebGL1 can only mipmap images which are a power of 2 in both dimensions.
-    // When targeting WebGL2 only, this if guard can be removed.
-    if (is_power_of_2(image.width) && is_power_of_2(image.height)) {
-        gl.generateMipmap(GL_TEXTURE_2D);
-        // GL_NEAREST_MIPMAP_LINEAR is the default. Consider switching to
-        // GL_LINEAR_MIPMAP_LINEAR for the best quality.
-        gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-        // GL_LINEAR is the default; make it explicit.
-        gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    } else {
-        // GL_LINEAR is the default; make it explicit.
-        gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    }
+    // All our textures meet this criterion.
+    gl.generateMipmap(GL_TEXTURE_2D);
 
-    // GL_REPEAT is the default; make it explicit.
-    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // These are the defaults.
+    // gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+    // gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    // gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
     return texture;
 }
@@ -67,8 +49,8 @@ export function create_texture_rgba(gl: WebGLRenderingContext, width: number, he
         null
     );
 
+    // The default value of GL_NEAREST_MIPMAP_LINEAR is only valid when a mipmap has been generated.
     gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     return texture;
 }
@@ -78,33 +60,6 @@ export function create_render_buffer(gl: WebGLRenderingContext, width: number, h
     gl.bindRenderbuffer(gl.RENDERBUFFER, buffer);
     gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width, height);
     return buffer;
-}
-
-// Depth textures are a WebGL2 feature. They can also be used in WebGL1 via an extension:
-// https://developer.mozilla.org/en-US/docs/Web/API/WEBGL_draw_buffers
-export function create_texture_depth(gl: WebGL2RenderingContext, width: number, height: number) {
-    let texture = gl.createTexture()!;
-    gl.bindTexture(GL_TEXTURE_2D, texture);
-    gl.texImage2D(
-        GL_TEXTURE_2D,
-        0,
-        GL_DEPTH_COMPONENT16,
-        width,
-        height,
-        0,
-        GL_DEPTH_COMPONENT,
-        GL_DATA_UNSIGNED_INT,
-        null
-    );
-
-    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    gl.texParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    return texture;
-}
-
-function is_power_of_2(value: number) {
-    return (value & (value - 1)) == 0;
 }
 
 export function generate_texture(
