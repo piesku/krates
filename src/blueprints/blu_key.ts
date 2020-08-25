b82182b5867ac82ec23b3ad687fa33a4f599f24a
Staś Małolepszy <stas@piesku.com>
2020-08-25 19:31:40 +0200

    Fix key shadow

--- src/blueprints/blu_key.ts
+++ src/blueprints/blu_key.ts
@@ -33,7 +33,7 @@ export function blueprint_key(game: Game, textured = false): Blueprint {
                 ],
             },
             {
-                Scale: [1.3, 1, 1.3],
+                Scale: [0.7, 0.7, 1],
                 Translation: [0, -0.4, 0],
                 Rotation: [-0.707, 0, 0, 0.707],
                 Using: [
