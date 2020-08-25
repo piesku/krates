77b38fc0238659f60993fec3662750fafad7cea3
Staś Małolepszy <stas@piesku.com>
2020-08-25 19:48:27 +0200

    Add snd_welcome (inactive)

---
+++ src/sounds/snd_welcome.ts
@@ -0,0 +1,24 @@
+import {AudioClip} from "../components/com_audio_source.js";
+
+export let snd_welcome: AudioClip = {
+    Tracks: [
+        {
+            Instrument: [
+                8,
+                false,
+                8,
+                8,
+                false,
+                "square",
+                9,
+                5,
+                [
+                    ["square", 8, 4, 3, 5, 8, false, false, 2, 7, 4],
+                    ["square", 8, 4, 4, 6, 9, true, false, 8, 8, 8],
+                ],
+            ],
+            Notes: [77],
+        },
+    ],
+    Exit: 0.1,
+};
