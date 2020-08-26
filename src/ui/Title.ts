1b33527f304033bd1c39f9aa4fea514d939a451e
Staś Małolepszy <stas@piesku.com>
2020-08-26 10:12:18 +0200

    Update the title to the new CSS

--- src/ui/Title.ts
+++ src/ui/Title.ts
@@ -3,37 +3,39 @@ import {Action} from "../actions.js";
 
 export function Title() {
     return html`
-        <div
-            class="screen"
-            style="
-                display: grid;
-                place-items: center;
-                grid-template-columns: repeat(5, 1fr);
-                grid-template-rows: repeat(6, 1fr);
-                font-style: italic;
-            "
-        >
-            <div style="grid-area: 1/1/3/3; font-size: 3em; animation: 0.3s ease-in both title;">
-                Oh
-            </div>
+        <div class="screen">
             <div
-                style="grid-area: 1/3/3/5; font-size: 4em; animation: 0.3s ease-in 0.5s both title;"
+                class="view"
+                style="
+                    grid-template-columns: repeat(5, 1fr);
+                    grid-template-rows: repeat(6, 1fr);
+                    font-style: italic;
+                "
             >
-                No!
-            </div>
-            <div style="grid-area: 3/1/4/2; animation: 0.2s ease-in 1s both title;">All</div>
-            <div style="grid-area: 4/1/5/2; animation: 0.2s ease-in 1.2s both title;">The</div>
-            <div
-                style="font-size: 2em; grid-area: 3/2/5/5; animation: 0.3s ease-in 1.4s both title;"
-            >
-                Textures
-            </div>
-            <div style="grid-area: 5/2/6/3; animation: 0.2s ease-in 1.9s both title;">Are</div>
-            <div
-                style="font-size: 4em; grid-area: 5/3/7/6; animation: 1s ease-in 2s both gone;"
-                onanimationend="$(${Action.TexturesAreGone})"
-            >
-                404!
+                <div
+                    style="grid-area: 1/1/3/3; font-size: 3em; animation: 0.3s ease-in both title;"
+                >
+                    Oh
+                </div>
+                <div
+                    style="grid-area: 1/3/3/5; font-size: 4em; animation: 0.3s ease-in 0.5s both title;"
+                >
+                    No!
+                </div>
+                <div style="grid-area: 3/1/4/2; animation: 0.2s ease-in 1s both title;">All</div>
+                <div style="grid-area: 4/1/5/2; animation: 0.2s ease-in 1.2s both title;">The</div>
+                <div
+                    style="font-size: 2em; grid-area: 3/2/5/5; animation: 0.3s ease-in 1.4s both title;"
+                >
+                    Textures
+                </div>
+                <div style="grid-area: 5/2/6/3; animation: 0.2s ease-in 1.9s both title;">Are</div>
+                <div
+                    style="font-size: 4em; grid-area: 5/3/7/6; animation: 1s ease-in 2s both gone;"
+                    onanimationend="$(${Action.TexturesAreGone})"
+                >
+                    404!
+                </div>
             </div>
         </div>
         <div class="screen">
