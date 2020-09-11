d4013a5be3a487b5a93104cef86c6dcc4aca9ee4
Michał Budzyński <michal@virtualdesign.pl>
2020-09-12 00:01:32 +0200

    palms

--- src/blueprints/blu_palm.ts
+++ src/blueprints/blu_palm.ts
@@ -1,3 +1,4 @@
+import {from_euler} from "../../common/quat.js";
 import {collide} from "../components/com_collide.js";
 import {render_textured_diffuse} from "../components/com_render_textured_diffuse.js";
 import {rigid_body} from "../components/com_rigid_body.js";
@@ -13,6 +14,7 @@ export function blueprint_palm(game: Game, textured = false): Blueprint {
             {
                 Translation: [0, 0.5, 0],
                 Scale: [0.5, 1, 0.5],
+                Rotation: from_euler([0, 0, 0, 0], -15, 0, 0),
                 Using: [
                     render_textured_diffuse(
                         game.MaterialTexturedDiffuse,
@@ -21,6 +23,19 @@ export function blueprint_palm(game: Game, textured = false): Blueprint {
                         [1, 1, 1, 1],
                         1,
                         texture
+                    ),
+                ],
+            },
+            {
+                // Shadow
+                Rotation: from_euler([0, 0, 0, 0], -90, 0, 0),
+                Scale: [0.3, 0.1, 1],
+                Translation: [0, -0.49, 0.2],
+                Using: [
+                    render_textured_diffuse(
+                        game.MaterialTexturedDiffuse,
+                        game.MeshQuad,
+                        game.Textures["shadow"]
                     ),
                 ],
             },
