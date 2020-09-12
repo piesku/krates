0fd214f7509d21e85b6240c90025ae2924f155ef
Staś Małolepszy <stas@piesku.com>
2020-09-13 01:23:28 +0200

    Show splash

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import {create_texture_from} from "../common/texture.js";
 import {dispatch} from "./actions.js";
 import {loop_start} from "./core.js";
 import {Game} from "./game.js";
-import {scene_title} from "./scenes/sce_title.js";
 
 let game = new Game();
 
@@ -13,9 +12,5 @@ window.$ = (...args) => dispatch(game, ...args);
 for (let texture of document.querySelectorAll("img")) {
     game.Textures[texture.id] = create_texture_from(game.Gl, texture);
 }
-
-// Skip splash for faster dev feedback.
-// scene_stage(game, 1);
-scene_title(game);
 
 loop_start(game);
